8ae6708df8adf6d06871f61e7d812633d2bad4d7
Saujal Karki <saujalkarki04@gmail.com>
2024-01-05 22:38:22 +0545

    db

--- 4-natours/dbConfig/dbConfig.js
+++ 4-natours/dbConfig/dbConfig.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 try {
   exports.connectdb = async (URI) => {
     await mongoose.connect(URI);
-    // console.log("successfully connected to database");
+    console.log("successfully connected to database");
   };
 } catch (err) {
   console.log(err);
