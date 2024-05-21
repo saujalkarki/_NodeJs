baa11551e463f3ff20e75ceba0051843e83ed56b
saujalkarki <saujalkarki04@gmail.com>
2024-05-21 22:10:22 +0545

    signup and login page

--- restartNode/app.js
+++ restartNode/app.js
@@ -5,13 +5,24 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   // res.send("<h1><i>We are in Home page right now.</i></h1>");
-  res.render("home.ejs");
+  res.render("home.ejs", { learned: "Hello World" });
+});
+
+app.get("/signup", (req, res) => {
+  res.render("./auth/signup.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("./auth/login.ejs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about.ejs");
 });
 
-app.listen(3000, (req, res) => {
-  console.log("Project ran successfully on port 3000");
+app.use(express.static("public/css"));
+
+const PORT = 3000;
+app.listen(PORT, (req, res) => {
+  console.log(`Project ran successfully on port ${PORT}`);
 });
